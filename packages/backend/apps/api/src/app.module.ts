73170b8379268bed6cce7cec59598516499fbcb2
George Taschina <g.taschina@georgetaschina.com>
2025-02-12 18:07:31 +0100

    refactor: removed unused code

--- packages/backend/apps/api/src/app.module.ts
+++ packages/backend/apps/api/src/app.module.ts
@@ -1,15 +1,13 @@
 import { TourModule } from '../../../libs/tour/src/tour.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CoreModule } from '@has-george-read-backend/core/core.module';
 import { ConfigModule } from '@nestjs/config';
 import { appConfigLoader } from './config/app-config.loader';
 
 @Module({
   imports: [CoreModule, TourModule, ConfigModule.forFeature(appConfigLoader)],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
   exports: [ConfigModule],
 })
 export class AppModule {}
